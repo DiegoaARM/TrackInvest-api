8c00a3b59df202bd5c732623896ee0619c9244ac
Juan Diego Duque <juandiegoduquejimenez@gmail.com>
2026-05-14 09:27:16 -0500

    Agregando el userExceptionHandler y arreglando endpoint de changeName

--- src/main/java/com/trackinvest/account/user/domain/exception/business/UserNotFoundException.java
+++ src/main/java/com/trackinvest/account/user/domain/exception/business/UserNotFoundException.java
@@ -5,6 +5,6 @@ import com.trackinvest.account.common.domain.exception.DomainException;
 public class UserNotFoundException extends DomainException {
 
     public UserNotFoundException() {
-        super(String.format("The user was not found."));
+        super("The user was not found.");
     }
 }
--- src/main/java/com/trackinvest/account/user/infrastructure/adapter/in/controller/UserController.java
+++ src/main/java/com/trackinvest/account/user/infrastructure/adapter/in/controller/UserController.java
@@ -2,14 +2,12 @@ package com.trackinvest.account.user.infrastructure.adapter.in.controller;
 
 import com.trackinvest.account.common.application.dto.ApiResponse;
 import com.trackinvest.account.user.application.ports.in.dto.user.GetUserResponseDTO;
+import com.trackinvest.account.user.application.ports.in.service.user.ChangeNamePort;
 import com.trackinvest.account.user.application.ports.in.service.user.GetMePort;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -20,6 +18,7 @@ import java.util.UUID;
 public class UserController {
 
     private final GetMePort getMePort;
+    private final ChangeNamePort changeNamePort;
 
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<GetUserResponseDTO>> getMe(
@@ -27,5 +26,14 @@ public class UserController {
     ) {
         GetUserResponseDTO userDTO = getMePort.execute(userId);
         return ResponseEntity.ok(ApiResponse.success(userDTO, "User information retrieved successfully"));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApiResponse<Void>> changeName(
+            @RequestAttribute("USER_ID") UUID userId,
+            @RequestParam String newFullName
+    ) {
+        changeNamePort.changeName(userId, newFullName);
+        return ResponseEntity.ok(ApiResponse.success(null, "User name updated successfully"));
     }
 }
